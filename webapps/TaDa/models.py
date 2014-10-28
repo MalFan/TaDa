7b25729c402cefc0111435ae8f95fefc313cb2e7
gabrevil <287620784@qq.com>
2014-10-28 14:20:06 -0400

    Update Models.py
    Add more models and field into Models.py

--- webapps/TaDa/models.py
+++ webapps/TaDa/models.py
@@ -4,12 +4,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-	user = models.OneToOneField(User)
+	user = models.OneToOneField(User, primary_key = True)
 	intro = models.CharField(max_length=255, blank=True)
 	photo = models.ImageField(
 			upload_to='profile-photos', 
 			default='profile-photos/user_default.png') # attrs to be modified
 	movies_liked = models.ManyToManyField(Movie, related_name='', blank=True) # attrs to be modified
+	movies_disliked = models.ManyToManyField(Movie, related_name='', blank=True) # attrs to be modified
 	users_followed = models.ManyToManyField(User, related_name='', blank=True) # attrs to be modified
 
 	def __unicode__(self):
@@ -19,18 +20,22 @@ class Profile(models.Model):
 class Movie(models.Model):
 	name = models.CharField(max_length=255)
 	year = models.CharField(max_length=4)
+	date = models.DateField()
+	isInTheater = models.NullBooleanField()
 	cover = models.ImageField(upload_to='movie-cover', blank=True) # attrs to be modified
-	# director = models.CharField(max_length=255)
-	# cast = models.ManyToManyField(Star, related_name='', blank=True) # attrs to be modified
+	director = models.ManyToManyField(Person, related_name = 'directing', blank=True)	
+	writer = models.ManyToManyField(Person, related_name = 'writing', blank=True)
+	cast = models.ManyToManyField(Person, related_name='acting', blank=True) # attrs to be modified
 	storyline = models.CharField(max_length=1024)
-	category = models.ManyToManyField(Category, related_name='', blank=True) # attrs to be modified
-	tag = models.ManyToManyField(Tag, related_name='', blank=True) # attrs to be modified
+	genres = models.ManyToManyField(Genre, related_name='including_movies', blank=True) # attrs to be modified
+	certificate = models.CharField(max_length=10, blank=True)
+	# tag = models.ManyToManyField(Tag, related_name='', blank=True) # attrs to be modified
 
 	def __unicode__(self):
 		return self.name
 
 
-class Category(models.Model):
+class Genre(models.Model):
 	name = models.CharField(max_length=255)
 
 	def __unicode__(self):
@@ -43,12 +48,21 @@ class Tag(models.Model):
 	def __unicode__(self):
 		return self.name
 
+class People(models.Model):
+	name = models.CharField(max_length=255)
+	dob = models.DateField()
+	bio = models.CharField(max_length=1024)	
+	photo = models.ImageField(upload_to='people', blank=True)
+
+	def __unicode__(self):
+		return self.name
 
 class Review(models.Model):
 	pub_time = models.DateTimeField(auto_now_add=True)
 	title = models.CharField(max_length=255)
 	text = models.CharField(max_length=8192)
-	publisher = models.ForeignKey(User)
+	movie = models.ForeignKey(Movie, related_name = 'including_reviews')
+	publisher = models.ForeignKey(User, related_name = 'publishing')
 
 	def __unicode__(self):
 		return self.title
@@ -57,8 +71,8 @@ class Review(models.Model):
 class Comment(models.Model):
 	pub_time = models.DateTimeField(auto_now_add=True)
 	text = models.CharField(max_length=4096)
-	publisher = models.ForeignKey(User)
-	review = models.ForeignKey(Review)
+	publisher = models.ForeignKey(User, related_name='publishing')
+	review = models.ForeignKey(Review, related_name = 'including_comments')
 
 	def __unicode__(self):
 		return self.text
