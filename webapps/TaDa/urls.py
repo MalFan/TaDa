badc9f72559ce11fb97b88c2435d2bc5980fff83
Desai Fan <dfan@andrew.cmu.edu>
2014-11-07 14:40:09 -0500

    Searching feature added.

--- webapps/TaDa/urls.py
+++ webapps/TaDa/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
 	url(r'^admin-search$', 'TaDa.views_admin.admin_search', name='admin_search'),
 	url(r'^admin-add-movie/(?P<movie_id>\d+)$', 'TaDa.views_admin.admin_add_movie', name='admin_add_movie'),
 	url(r'^admin-add-person/(?P<person_id>\d+)$', 'TaDa.views_admin.admin_add_person', name='admin_add_person'),
+	
 	url(r'^$','TaDa.views.home'),
 	url(r'^recommend-movie$','TaDa.views.recommend_movie'),
 	url(r'^search$','TaDa.views.search'),
--- webapps/TaDa/views.py
+++ webapps/TaDa/views.py
@@ -15,6 +15,23 @@ def recommend_movie(request):
 
 def search(request):
 	context = {}
+	keyword = request.GET['keyword']
+	movies = Movie.objects.filter(title__contains = keyword)
+	context['movie_combos'] = []
+	for m in movies:
+		movie_combo = {
+				'imdb_id' : m.imdb_id,
+				'title' : m.title,
+				'year' : m.year,
+				'cover' : m.cover,
+				'director_list' : m.director_list.all(),
+				'writer_list' : m.writer_list.all(),
+				'cast_list' : m.cast_list.all()[:15],
+				'storyline' : m.storyline,
+				'genre_list' : m.genre_list.all(),
+				'certificate' : m.certificate}
+		context['movie_combos'].append(movie_combo)
+
 	return render(request, 'search.html', context)
 
 
