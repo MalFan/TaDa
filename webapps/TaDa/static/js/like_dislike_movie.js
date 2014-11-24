9f4bfbd9382433b2b5e8c0de2f7c556c6beac2f0
gabrevil <287620784@qq.com>
2014-11-24 14:50:52 -0500

    Bug fixed
    Limit user access for like, dislike, comment before login

--- webapps/TaDa/static/js/like_dislike_movie.js
+++ webapps/TaDa/static/js/like_dislike_movie.js
@@ -7,63 +7,80 @@ $(document).ready( function() {
 });
 
 function likeAjax() {
-	$( "#like-btn" ).click(function(e) {	
-		var btnHref = $( this ).attr( "href" );
-		$.ajax(
-		{
-			url: btnHref,
-			success:function(response) 
+	$( "#like-btn" ).click(function(e) {
+		if(isLogin()){
+			var btnHref = $( this ).attr( "href" );
+			$.ajax(
 			{
-				// alert( "success" );
-				var currentLikes = $( "#like-badge-span" ).html();
-				var newLikes = parseInt(currentLikes, 10) + parseInt(response, 10);
-				$( "#like-badge-span" ).html( newLikes );
-				if ( response == "1" ) {
-					$( "#like-btn" ).removeClass( "btn-default" ).addClass( "btn-info" );
-					$( "#like-text-span" ).html( "Unlike" );
-				} else {
-					$( "#like-btn" ).removeClass( "btn-info" ).addClass( "btn-default" );
-					$( "#like-text-span" ).html( "Like" );
+				url: btnHref,
+				success:function(response) 
+				{
+					// alert( "success" );
+					var currentLikes = $( "#like-badge-span" ).html();
+					var newLikes = parseInt(currentLikes, 10) + parseInt(response, 10);
+					$( "#like-badge-span" ).html( newLikes );
+					if ( response == "1" ) {
+						$( "#like-btn" ).removeClass( "btn-default" ).addClass( "btn-info" );
+						$( "#like-text-span" ).html( "Unlike" );
+					} else {
+						$( "#like-btn" ).removeClass( "btn-info" ).addClass( "btn-default" );
+						$( "#like-text-span" ).html( "Like" );
+					}
+						
+				},
+				error: function() 
+				{
+				    //if fails 
+				    // alert( "error" );     
 				}
-					
-			},
-			error: function() 
-			{
-			    //if fails 
-			    // alert( "error" );     
-			}
-		});
+			});			
+		}else{
+			$('.login-popup').modal();
+		}
 		e.preventDefault(); //STOP default action
 	});
 }
 
 function dislikeAjax() {
 	$( "#dislike-btn" ).click(function(e) {	
-		var btnHref = $( this ).attr( "href" );
-		$.ajax(
-		{
-			url: btnHref,
-			success:function(response) 
+		if(isLogin()){
+			var btnHref = $( this ).attr( "href" );
+			$.ajax(
 			{
-				// alert( "success" );
-				var currentDislikes = $( "#dislike-badge-span" ).html();
-				var newDislikes = parseInt(currentDislikes, 10) + parseInt(response, 10);
-				$( "#dislike-badge-span" ).html( newDislikes );
-				if ( response == "1" ) {
-					$( "#dislike-btn" ).removeClass( "btn-default" ).addClass( "btn-info" );
-					$( "#dislike-text-span" ).html( "Undislike" );
-				} else {
-					$( "#dislike-btn" ).removeClass( "btn-info" ).addClass( "btn-default" );
-					$( "#dislike-text-span" ).html( "Dislike" )
+				url: btnHref,
+				success:function(response) 
+				{
+					// alert( "success" );
+					var currentDislikes = $( "#dislike-badge-span" ).html();
+					var newDislikes = parseInt(currentDislikes, 10) + parseInt(response, 10);
+					$( "#dislike-badge-span" ).html( newDislikes );
+					if ( response == "1" ) {
+						$( "#dislike-btn" ).removeClass( "btn-default" ).addClass( "btn-info" );
+						$( "#dislike-text-span" ).html( "Undislike" );
+					} else {
+						$( "#dislike-btn" ).removeClass( "btn-info" ).addClass( "btn-default" );
+						$( "#dislike-text-span" ).html( "Dislike" )
+					}
+				},
+				error: function() 
+				{
+				    //if fails 
+				    // alert( "error" );     
 				}
-			},
-			error: function() 
-			{
-			    //if fails 
-			    // alert( "error" );     
-			}
-		});
+			});
+		}else{
+			$('.login-popup').modal();
+		}
 		e.preventDefault(); //STOP default action
 	});
 }
 
+function isLogin(){
+	var status = $("#log-btn").attr("status");
+	console.log(status);
+	if(status == "loggedin"){
+		return true;
+	}else{
+		return false;
+	}
+}
--- webapps/TaDa/static/js/write_comment.js
+++ webapps/TaDa/static/js/write_comment.js
@@ -8,38 +8,42 @@ $(document).ready( function() {
 
 function writeCommentAjax() {
 	$("#review-form").submit( function( e ) {
-		var input = $("#id_text")
-		var content = input.val();
-		console.log(content);
-		if (!content.trim()) {
-        // is empty or whitespace
-		  input.attr("placeholder","Cannot post an empty comment.")
-		  input.attr("class","form-control input_warning")
+		if(isLogin()){
+			var input = $("#id_text")
+			var content = input.val();
+			console.log(content);
+			if (!content.trim()) {
+	        // is empty or whitespace
+			  input.attr("placeholder","Cannot post an empty comment.")
+			  input.attr("class","form-control input_warning")
+			}else{
+				var postData = $(this).serializeArray();
+				var formURL = $(this).attr("action");
+				$.ajax(
+				{
+					url : formURL,
+					type: "POST",
+					data : postData,
+					dataType: "html",
+					success:function(html) 
+					{
+						// alert( "success" );
+						$('ul.comment-list').prepend(html);		    
+					},
+					error: function() 
+					{
+						//if fails
+						alert( "error" );      
+					}
+				});
+				 //STOP default action
+				input.removeAttr("placeholder")
+			  	input.removeAttr('value'); 
+			  	input.attr("class","form-control")         
+			  	input.val('');
+			}
 		}else{
-			var postData = $(this).serializeArray();
-			var formURL = $(this).attr("action");
-			$.ajax(
-			{
-				url : formURL,
-				type: "POST",
-				data : postData,
-				dataType: "html",
-				success:function(html) 
-				{
-					// alert( "success" );
-					$('ul.comment-list').prepend(html);		    
-				},
-				error: function() 
-				{
-					//if fails
-					alert( "error" );      
-				}
-			});
-			 //STOP default action
-			input.removeAttr("placeholder")
-		  	input.removeAttr('value'); 
-		  	input.attr("class","form-control")         
-		  	input.val('');
+			$('.login-popup').modal();
 		}
 		e.preventDefault();
 
@@ -47,3 +51,12 @@ function writeCommentAjax() {
 
 }
 
+function isLogin(){
+	var status = $("#log-btn").attr("status");
+	console.log(status);
+	if(status == "loggedin"){
+		return true;
+	}else{
+		return false;
+	}
+}
