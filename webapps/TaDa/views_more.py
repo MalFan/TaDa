91fd14b6779e9656db2c256dde19f8b627d34adb
gabrevil <287620784@qq.com>
2014-11-30 17:13:22 -0500

    Complete profile detail templates
    Complete full templates of "more.." in profile page.

--- webapps/TaDa/views_more.py
+++ webapps/TaDa/views_more.py
@@ -126,12 +126,135 @@ def movie_people_who_liked_list(request,movie_id):
 
 def profile_movie_list(request, view_user_id):
 	context = {}
+	regis_form = RegistrationForm()
+	login_form = LoginForm()
+	context['regis_form'] = regis_form
+	context['login_form'] = login_form
+	context['search_form'] = SearchForm()
+	context['request'] = request
+	context['user'] = request.user
+	user_be_view = get_object_or_404(User,id = view_user_id)
+	print user_be_view.username
+
+	if Profile.objects.filter(user = user_be_view).count() == 0:
+		profile = Profile(user = user_be_view)
+		profile.save()
+
+	if request.user == user_be_view:
+		context['view_user'] = request.user
+		profile = get_object_or_404(Profile, user = user_be_view)
+		intro_form = IntroForm(instance = profile)
+		photo_form = PhotoForm(instance = profile)
+		context['intro_form'] = intro_form
+		context['photo_form'] = photo_form
+	else:
+		context['view_user'] = user_be_view
+
+	context['reviews'] = Review.objects.filter(publisher = user_be_view).order_by('id').reverse()
+	
+	if Profile.objects.filter(user = user_be_view).count() > 0:
+		profile = Profile.objects.get(user = user_be_view)
+		context['profile'] = profile
+		# print profile.photo
+	# context['photo_form'] = PhotoForm()
+	context['search_form'] = SearchForm()
+	user_be_followed = get_object_or_404(User, id = view_user_id)
+	if request.user.username:
+		profile_of_login_user = get_object_or_404(Profile, user = request.user)
+		if user_be_followed in profile_of_login_user.users_followed.all():
+			context['follow_text'] = 'unfollow'
+		else:
+			context['follow_text'] = 'follow'
+	else:
+		context['follow_text'] = 'follow'
 	return render(request, 'profile_movie_list.html', context)
 
 def profile_review_list(request, view_user_id):
 	context = {}
+	regis_form = RegistrationForm()
+	login_form = LoginForm()
+	context['regis_form'] = regis_form
+	context['login_form'] = login_form
+	context['search_form'] = SearchForm()
+	context['request'] = request
+	context['user'] = request.user
+	user_be_view = get_object_or_404(User,id = view_user_id)
+	print user_be_view.username
+
+	if Profile.objects.filter(user = user_be_view).count() == 0:
+		profile = Profile(user = user_be_view)
+		profile.save()
+
+	if request.user == user_be_view:
+		context['view_user'] = request.user
+		profile = get_object_or_404(Profile, user = user_be_view)
+		intro_form = IntroForm(instance = profile)
+		photo_form = PhotoForm(instance = profile)
+		context['intro_form'] = intro_form
+		context['photo_form'] = photo_form
+	else:
+		context['view_user'] = user_be_view
+
+	context['reviews'] = Review.objects.filter(publisher = user_be_view).order_by('id').reverse()
+	
+	if Profile.objects.filter(user = user_be_view).count() > 0:
+		profile = Profile.objects.get(user = user_be_view)
+		context['profile'] = profile
+		# print profile.photo
+	# context['photo_form'] = PhotoForm()
+	context['search_form'] = SearchForm()
+	user_be_followed = get_object_or_404(User, id = view_user_id)
+	if request.user.username:
+		profile_of_login_user = get_object_or_404(Profile, user = request.user)
+		if user_be_followed in profile_of_login_user.users_followed.all():
+			context['follow_text'] = 'unfollow'
+		else:
+			context['follow_text'] = 'follow'
+	else:
+		context['follow_text'] = 'follow'
 	return render(request, 'profile_review_list.html', context)
 
 def profile_following_list(request, view_user_id):
 	context = {}
+	regis_form = RegistrationForm()
+	login_form = LoginForm()
+	context['regis_form'] = regis_form
+	context['login_form'] = login_form
+	context['search_form'] = SearchForm()
+	context['request'] = request
+	context['user'] = request.user
+	user_be_view = get_object_or_404(User,id = view_user_id)
+	print user_be_view.username
+
+	if Profile.objects.filter(user = user_be_view).count() == 0:
+		profile = Profile(user = user_be_view)
+		profile.save()
+
+	if request.user == user_be_view:
+		context['view_user'] = request.user
+		profile = get_object_or_404(Profile, user = user_be_view)
+		intro_form = IntroForm(instance = profile)
+		photo_form = PhotoForm(instance = profile)
+		context['intro_form'] = intro_form
+		context['photo_form'] = photo_form
+	else:
+		context['view_user'] = user_be_view
+
+	context['reviews'] = Review.objects.filter(publisher = user_be_view).order_by('id').reverse()
+	
+	if Profile.objects.filter(user = user_be_view).count() > 0:
+		profile = Profile.objects.get(user = user_be_view)
+		context['profile'] = profile
+		# print profile.photo
+	# context['photo_form'] = PhotoForm()
+	context['search_form'] = SearchForm()
+	user_be_followed = get_object_or_404(User, id = view_user_id)
+	if request.user.username:
+		profile_of_login_user = get_object_or_404(Profile, user = request.user)
+		if user_be_followed in profile_of_login_user.users_followed.all():
+			context['follow_text'] = 'unfollow'
+		else:
+			context['follow_text'] = 'follow'
+	else:
+		context['follow_text'] = 'follow'
 	return render(request, 'profile_following_list.html', context)
